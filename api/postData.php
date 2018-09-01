39b21228d46489d909559bdd042f9b5e19b575dd
Bayu Hafiz <bayuhafiz@hotmail.com>
2018-09-01 19:57:25 +0700

    updated: Extended chart

---
+++ api/postData.php
@@ -0,0 +1,17 @@
+<?php
+include "conn.php";
+if (!empty($_POST['id'])) {
+    $query = $db->query("SELECT * FROM perolehan WHERE id = {$_POST['id']}");
+
+    if ($query->num_rows > 0) {
+        $res = $query->fetch_assoc();
+        $data['status'] = 'ok';
+        $data['result'] = $res;
+    } else {
+        $data['status'] = 'err';
+        $data['result'] = '';
+    }
+
+    //returns data as JSON format
+    echo json_encode($data);
+}
