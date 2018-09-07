1a3f3537535731b6cb37b1c9649d8dee3160e5d3
Bayu Hafiz <bayuhafiz@hotmail.com>
2018-09-07 11:31:32 +0700

    debug: Chart #2

--- api/postData.php
+++ api/postData.php
@@ -2,10 +2,8 @@
 include "conn.php";
 include "inc/chromePhp.php";
 
-// ChromePhp::log($_POST['tanggal']);
-
 if (!empty($_POST['tanggal'])) {
-    $query = $db->query("SELECT * FROM perolehan WHERE tanggal = '{$_POST['tanggal']}' AND tipe_produk = 'fl' OR tipe_produk = 'cl' OR tipe_produk = 'as' OR tipe_produk = 'ts'");
+    $query = $db->query("SELECT * FROM perolehan WHERE tanggal = '{$_POST['tanggal']}' AND (tipe_produk = 'fl' OR tipe_produk = 'cl' OR tipe_produk = 'as' OR tipe_produk = 'ts')");
     $rows = array();
 
     if ($query->num_rows > 0) {
