efb3d2855dbc352817723049512ad13635461174
buyung1985 <42860180+buyung1985@users.noreply.github.com>
2018-09-25 13:56:06 +0700

    Add files via upload

--- api/postWs.php
+++ api/postWs.php
@@ -9,9 +9,13 @@ if (!empty($_POST['data_type'])) {
     if ($dt == "FL" || $dt == "CL" || $dt == "AS" || $dt == "TS") {
         $query_string = "SELECT * FROM shipment WHERE week = {$_POST['data_week']} AND tanggal = '{$_POST['data_date']}' AND (produk = 'fl' OR produk = 'cl' OR produk = 'as' OR produk = 'ts')";
         $query = $db->query($query_string);
+        $query_string_2 = "SELECT MAX(updated_at) AS updated_at WHERE week = {$_POST['data_week']} AND tanggal = '{$_POST['data_date']}' AND (produk = 'fl' OR produk = 'cl' OR produk = 'as' OR produk = 'ts')";
+        $query_2 = $db->query($query_string_2);
     } else {
-        $query_string = "SELECT * FROM shipment WHERE week = {$_POST['data_week']}  AND tanggal = '{$_POST['data_date']}' AND (produk = 'pn' OR produk = 'rc' OR produk = 'vn')";
+        $query_string = "SELECT * FROM shipment WHERE week = {$_POST['data_week']}  AND tanggal = '{$_POST['data_date']}'AND (produk = 'pn' OR produk = 'rc' OR produk = 'vn')";
         $query = $db->query($query_string);
+         $query_string_2 = "SELECT MAX(updated_at) AS updated_at WHERE week = {$_POST['data_week']}  AND tanggal = '{$_POST['data_date']}'AND (produk = 'pn' OR produk = 'rc' OR produk = 'vn')";
+        $query_2 = $db->query($query_string_2);
     }
 
     // ChromePhp::log($query);
@@ -20,9 +24,10 @@ if (!empty($_POST['data_type'])) {
         while ($r = mysqli_fetch_assoc($query)) {
             $rows[] = $r;
         }
-
+        
         $data['status'] = 'ok';
         $data['result'] = $rows;
+        $data['updated_at'] = $query_2['updated_at'];
         $data['result'][0]['data_type'] = $_POST['data_type'];
     } else {
         $data['status'] = 'err';
